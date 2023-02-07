d25319ff683ac7527e96b3d2c68964dc117667d5
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-07 19:15:17 +0530

    Create Minimum Absolute Difference.py

---
+++ Minimum Absolute Difference.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    mini=max(arr)
+    for i in range(len(arr)-1):
+        mini=min(mini,abs(arr[i]-arr[i+1]))
+    return mini
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
