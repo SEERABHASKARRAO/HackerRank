91ea2b3dc3866bf98ec00a0fb7d69efa406c2acd
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-11 10:36:11 +0530

    Create Migratory Birds.py

---
+++ Migratory Birds.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    mini=9999
+    maxi=0
+    for i in list(set(arr)):
+        maxi=max(maxi,arr.count(i))
+        if(arr.count(mini)==maxi):
+         mini=min(mini,i)
+        else:
+            mini=i
+    return mini
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
