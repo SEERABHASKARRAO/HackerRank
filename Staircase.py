773459a0116d8dc958b6245262a1a9bf5ea2cc1b
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-03-17 22:28:03 +0530

    Update Staircase.py

--- Staircase.py
+++ Staircase.py
@@ -4,11 +4,7 @@ import random
 import re
 import sys
 
-#
-# Complete the 'staircase' function below.
-#
-# The function accepts INTEGER n as parameter.
-#
+
 
 def staircase(n):
     for i in range(n):
