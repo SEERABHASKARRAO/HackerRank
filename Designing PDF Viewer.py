16c71ce3900d57a54bdc587ab880c7ac5882aa1a
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-04-07 08:30:15 +0530

    Update Designing PDF Viewer.py

--- Designing PDF Viewer.py
+++ Designing PDF Viewer.py
@@ -7,7 +7,6 @@ import re
 import sys
 
 # Complete the 'designerPdfViewer' function below.
-
 # The function is expected to return an INTEGER.
 # The function accepts following parameters:
 #  1. INTEGER_ARRAY h
